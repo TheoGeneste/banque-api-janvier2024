6c279c38bf427d68f3af4377a1f2f9efdaa071e8
theo geneste <theo.boxe@hotmail.fr>
2024-05-16 16:59:40 +0200

    Ajout des routes pour récuperer mes Models par leur ID

--- Controllers/CompteController.js
+++ Controllers/CompteController.js
@@ -10,6 +10,16 @@ class CompteController {
             result.json({error : 'Une erreur est surevnue lors de la récupération des comptes'});
         }
     }
+
+    async getCompteByID(request, result){
+        try {
+            const compte = await CompteService.getCompteByID(request.params.id);
+            result.json(compte);
+        } catch (error) {
+            result.status(500);
+            result.json({error : 'Une erreur est surevnue lors de la récupération du compte'});
+        }
+    }
 }
 
 module.exports = new CompteController();
--- Controllers/TransactionController.js
+++ Controllers/TransactionController.js
@@ -11,6 +11,16 @@ class TransactionController {
             result.json({error : "Une erreur est survenue lors de la récupération des transactions"})
         }
     }
+
+    async getTransactionByID(request, result){
+        try {
+            const transaction = await TransactionService.getTransactionByID(request.params.id);
+            result.json(transaction);
+        } catch (error) {
+            result.status(500);
+            result.json({error : "Une erreur est survenue lors de la récupération de la transaction"})
+        }
+    }
 }
 
 module.exports = new TransactionController();
